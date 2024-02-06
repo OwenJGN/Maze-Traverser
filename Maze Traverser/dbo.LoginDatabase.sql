71805cf982c8bbc11f40dc3af2e7a490c31d93dd
Owen Gibson <owenjgibson@gmail.com>
2024-02-06 15:02:45 +0000

    Editing files organisation

--- Maze Traverser/dbo.LoginDatabase.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[ID] INT NOT NULL PRIMARY KEY, 
-    [Username] TEXT NOT NULL, 
-    [Email] TEXT NOT NULL, 
-    [Password] TEXT NOT NULL, 
-    [Access Level] TEXT NOT NULL
-)
